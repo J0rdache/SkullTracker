b69fd197c7b1903e36e084f196e1906c1ec3ed05
Jordan Reynolds <jdreynolds586@gmail.com>
2025-10-22 10:25:21 -0700

    Fixes to install script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -O https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_alt.xml && virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+curl -O https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_alt.xml && python3 -m venv .env && source .env/bin/activate && pip install -r requirements.txt
