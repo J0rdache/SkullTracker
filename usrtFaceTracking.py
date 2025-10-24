7fcb76bf44f1ced0352c9321ada30249c60593f6
Jordan Reynolds <jdreynolds586@gmail.com>
2025-10-24 12:45:15 -0700

    .

--- usrtFaceTracking.py
+++ usrtFaceTracking.py
@@ -161,8 +161,6 @@ class FaceTracker:
                 self.lastGraceTime = None
                 print("Return to center!")
                 return 401
-        else:
-            self.lastGraceTime = None
         if self.locked:
             if self.targetAvgX > self.camInfo['width'] // 2 - self.CenterWidth // 2 and self.targetAvgX < self.camInfo['width'] // 2 + self.CenterWidth // 2:
                 return 0
